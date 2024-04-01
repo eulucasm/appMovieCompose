78a3ecd313d14eccc84cf98ac7b2fe600aafbeee
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2024-04-01 16:28:19 -0300

    UseCase GetPopularMoviesUseCase

---
+++ app/src/main/java/br/com/movieapp/moviePopularFeature/domain/usecase/GetPopularMoviesUseCase.kt
@@ -0,0 +1,28 @@
+package br.com.movieapp.moviePopularFeature.domain.usecase
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.moviePopularFeature.domain.repository.MoviePopularRepository
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+
+interface GetPopularMoviesUseCase {
+    operator fun invoke(): Flow<PagingData<Movie>>
+}
+
+class GetPopularMoviesUseCaseImpl @Inject constructor(
+    private val repository: MoviePopularRepository
+) : GetPopularMoviesUseCase {
+
+
+    override fun invoke(): Flow<PagingData<Movie>> {
+        return repository.getPopularMovies(
+            pagingConfig = PagingConfig(
+                pageSize = 20,
+                initialLoadSize = 20
+            )
+        )
+    }
+
+}
