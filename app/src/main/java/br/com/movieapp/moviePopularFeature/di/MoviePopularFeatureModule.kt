fc34cafbfd70a822f516c381363fe62577b220d2
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2024-04-01 17:14:01 -0300

    implementenaod MoviePopularFeatureModule

---
+++ app/src/main/java/br/com/movieapp/moviePopularFeature/di/MoviePopularFeatureModule.kt
@@ -0,0 +1,38 @@
+package br.com.movieapp.moviePopularFeature.di
+
+import br.com.movieapp.core.data.remote.MovieService
+import br.com.movieapp.moviePopularFeature.data.repository.MoviePopularRepositoryImpl
+import br.com.movieapp.moviePopularFeature.data.source.MoviePopularRemoteDataSourceImpl
+import br.com.movieapp.moviePopularFeature.domain.repository.MoviePopularRepository
+import br.com.movieapp.moviePopularFeature.domain.source.MoviePopularRemoteDataSource
+import br.com.movieapp.moviePopularFeature.domain.usecase.GetPopularMoviesUseCase
+import br.com.movieapp.moviePopularFeature.domain.usecase.GetPopularMoviesUseCaseImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MoviePopularFeatureModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieDataSource(service: MovieService): MoviePopularRemoteDataSource {
+        return MoviePopularRemoteDataSourceImpl(service = service)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(remoteDataSource: MoviePopularRemoteDataSource): MoviePopularRepository {
+        return MoviePopularRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMoviesPopularUseCase(moviePopularRepository: MoviePopularRepository): GetPopularMoviesUseCase {
+        return GetPopularMoviesUseCaseImpl(repository = moviePopularRepository)
+    }
+
+}
