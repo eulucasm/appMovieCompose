83e1a20461025c1892869b3dc79e0f422f05edc9
eulucasm <lucaspoletis@gmail.com>
2024-04-05 01:41:34 -0300

    finalizando tela principal

--- app/src/main/java/br/com/movieapp/core/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/br/com/movieapp/core/presentation/navigation/NavigationGraph.kt
@@ -1,9 +1,12 @@
 package br.com.movieapp.core.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import br.com.movieapp.moviePopularFeature.presentation.MoviePopularScreen
+import br.com.movieapp.moviePopularFeature.presentation.MoviePopularViewModel
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
@@ -14,6 +17,15 @@ fun NavigationGraph(navController: NavHostController) {
     ) {
         composable(BottomNavItem.MoviePopular.route) {
 
+            val viewModel: MoviePopularViewModel = hiltViewModel()
+            val uiState = viewModel.uiState
+
+            MoviePopularScreen(
+                uiState = uiState,
+                navigateToDetailMovie = {
+
+                }
+            )
         }
         composable(BottomNavItem.MovieSearch.route) {
 
--- app/src/main/java/br/com/movieapp/moviePopularFeature/presentation/MoviePopularViewModel.kt
+++ app/src/main/java/br/com/movieapp/moviePopularFeature/presentation/MoviePopularViewModel.kt
@@ -1,5 +1,6 @@
 package br.com.movieapp.moviePopularFeature.presentation
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -10,12 +11,20 @@ import br.com.movieapp.moviePopularFeature.domain.usecase.GetPopularMoviesUseCas
 import br.com.movieapp.moviePopularFeature.presentation.state.MoviePopularState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
 class MoviePopularViewModel @Inject constructor(
     getPopularMoviesUseCase: GetPopularMoviesUseCase
 ) : ViewModel() {
 
+//      Maneira comum utilizada quando trabalhamos com XML
+//    private val _uiState2 = MutableStateFlow(MoviePopularState())
+//    val uiState2: StateFlow<MoviePopularState> = _uiState2.asStateFlow()
+
+    //Compose State
     var uiState by mutableStateOf(MoviePopularState())
         private set
 
