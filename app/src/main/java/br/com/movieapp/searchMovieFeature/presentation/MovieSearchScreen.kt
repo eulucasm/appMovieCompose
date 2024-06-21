bce3a50b8ae846d7e88dc4e4872409d78e789c7b
eulucasm <lucaspoletis@gmail.com>
2024-06-20 21:43:25 -0300

    view e navegation

---
+++ app/src/main/java/br/com/movieapp/searchMovieFeature/presentation/MovieSearchScreen.kt
@@ -0,0 +1,52 @@
+package br.com.movieapp.searchMovieFeature.presentation
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.paging.compose.collectAsLazyPagingItems
+import br.com.movieapp.R
+import br.com.movieapp.searchMovieFeature.presentation.components.SearchContent
+import br.com.movieapp.searchMovieFeature.presentation.state.MovieSearchState
+import br.com.movieapp.ui.theme.black
+import br.com.movieapp.ui.theme.white
+
+@Composable
+fun MovieSearchScreen(
+    uiState: MovieSearchState,
+    onEvent: (MovieSearchEvent) -> Unit,
+    onFetch: (String) -> Unit,
+    navigateToDetailsMovie: (Int) -> Unit
+) {
+    val pagingMovies = uiState.movies.collectAsLazyPagingItems()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.search_movies),
+                        color = white
+                    )
+                },
+                backgroundColor = black
+            )
+        },
+        content = { paddingValues ->
+            SearchContent(
+                paddingValues = paddingValues,
+                pagingMovies = pagingMovies,
+                query = uiState.query,
+                onSearch = {
+                    onFetch(it)
+                },
+                onEvent = {
+                    onEvent(it)
+                },
+                onDetail = { movieId ->
+                    navigateToDetailsMovie(movieId)
+                }
+            )
+        }
+    )
+}
